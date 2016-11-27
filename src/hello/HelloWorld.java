b7723e336addd8ffc903350482af01cd4a4b95c7
jaman123 <jaman.taneya1188@gmail.com>
2016-11-27 10:04:01 -0600

    initial setup

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int number1= 10;
+		int number2= 20;
+	    int total = number1 + number2;
+	    System.out.println(total);
+	}
+
+}
